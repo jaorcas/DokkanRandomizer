f379f4e515bf57a50b932ed0c44b4b199bd0c77c
Javier Ortega <jaorcas_10@hotmail.com>
2025-01-31 19:02:52 +0100

    fix heroes muy largo

--- Constants.ts
+++ Constants.ts
@@ -100,7 +100,7 @@ const Constants = {
     { id: 15, name: "Ginyu Forces" } as CategoriasItem,
     { id: 94, name: "Uncontrollable Power" } as CategoriasItem,
     { id: 93, name: "Demonic Power" } as CategoriasItem,
-    { id: 95, name: "Earth Protecting Heroes" } as CategoriasItem,
+    { id: 95, name: "Earth-Protecting H." } as CategoriasItem,
     { id: 96, name: "Successors" } as CategoriasItem,
 
   ] as CategoriasItem[],
